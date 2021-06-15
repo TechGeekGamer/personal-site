4ec2e3ebdd21cbdddb4e7c3fb83de3d8eec6cec4
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-15 03:07:39 -0700

    add router

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/Projects.vue')
+  },
+  {
+    path: '/other-projects',
+    name: 'Other Projects',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/OtherProjects.vue')
+  },
+  {
+    path: '/links',
+    name: 'Links',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/Links.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
