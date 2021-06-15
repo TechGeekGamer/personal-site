31e18b006d40a410db8e88785892d96db7b6e0a4
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-15 03:08:03 -0700

    Add src files

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About Me</b-nav-item>
+          <b-nav-item to="/projects">My Projects</b-nav-item>
+          <b-nav-item to="/other-projects">Other Projects</b-nav-item>
+          <b-nav-item to="/links">Links</b-nav-item>
+        </b-navbar-nav>
+        
+      </b-collapse>
+    </b-navbar>
+    <br>
+    <transition name="fade">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .25s;
+}
+
+.fade-enter-active {
+  transition-delay: .25s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+</style>
