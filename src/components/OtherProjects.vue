f4349a94a369bb597141b0e2e498f5bed13eb74d
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-15 03:07:15 -0700

    Add components

---
+++ src/components/OtherProjects.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <b-card-group deck>
+
+    <b-card title="DeepSea CFW">
+    <b-card-img src="https://cdn.discordapp.com/attachments/703315673534693467/847563949725581322/3838195-ai_5.png" alt="DeepSea CFW Logo" class="p-0"></b-card-img>
+    <br>
+    <br>
+      <b-card-text>
+          Deepsea is a new All-in-One CFW package for the Nintendo Switch. You can learn more at <a href="https://github.com/Team-Neptune/DeepSea">https://github.com/Team-Neptune/DeepSea</a>
+      </b-card-text>
+    </b-card>
+
+    <b-card title="Korral-JS">
+    <b-card-img src="https://cdn.discordapp.com/avatars/709980094717886529/c511814da6b09a742b652c89d335129c.webp?size=512" alt="BanSync Logo" class="w-25 rounded-circle"></b-card-img>
+    <br>
+    <br>
+      <b-card-text>
+          This is the official Discord bot for the <a href="https://discord.gg/VkaRjYN">Team Neptune Discord</a> server. The source is available <a href="https://github.com/Team-Neptune/Korral-JS">here</a>.
+      </b-card-text>
+    </b-card>
+
+    <b-card title="SlashNotes">
+    <b-card-img src="/slashnotes.png" alt="SlashNotes Logo" class="w-25 rounded-circle"></b-card-img>
+    <br>
+    <br>
+      <b-card-text>
+          SlashNotes is a basic bot that you can use to store notes using Discord's Slash Commands UI. It was mainly a side project I did in my free time. You can <a href="https://discord.com/oauth2/authorize?client_id=799921906047647744&scope=bot%20applications.commands">invite it</a> or <a href="https://github.com/TechGeekGamer/SlashNotes">view the source code</a>.
+      </b-card-text>
+    </b-card>
+    
+  </b-card-group>
+</div>
+</template>
+<script>
+export default {
+    name:"OtherProjects"
+}
+</script>
---
+++ src/components/Projects.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <b-card-group deck>
+
+    <b-card title="SlashRoles">
+    <b-card-img src="/slashroles.png" alt="SlashRoles Logo" class="w-25 rounded-circle"></b-card-img>
+    <br>
+    <br>
+      <b-card-text>
+          SlashRoles is a slash command bot that lets you setup options in the Slash Command menu to assign roles. By running <code>/create</code>, you can select a role to assign, and the title and description to show in the Slash Command menu. When someone runs that command, they'll be granted the role you selected, (if provided) and a custom message shown. You can remove an option by using <code>/delete</code>, and the data related to it will be deleted.
+      </b-card-text>
+    </b-card>
+
+    <b-card title="BanSync">
+    <b-card-img src="/bansync.png" alt="BanSync Logo" class="w-25 rounded-circle"></b-card-img>
+    <br>
+    <br>
+      <b-card-text>
+          Help keep out possible troublemakers from your server by knowing user's previous bans, or by linking your servers together to sync bans within them. Learn more at <a href="https://bansync.dcode.site">https://bansync.dcode.site</a>
+      </b-card-text>
+    </b-card>
+
+    <b-card title="SlashNotes">
+    <b-card-img src="/slashnotes.png" alt="SlashNotes Logo" class="w-25 rounded-circle"></b-card-img>
+    <br>
+    <br>
+      <b-card-text>
+          SlashNotes is a basic bot that you can use to store notes using Discord's Slash Commands UI. It was mainly a side project I did in my free time. You can <a href="https://discord.com/oauth2/authorize?client_id=799921906047647744&scope=bot%20applications.commands">invite it</a> or <a href="https://github.com/TechGeekGamer/SlashNotes">view the source code</a>.
+      </b-card-text>
+    </b-card>
+    
+  </b-card-group>
+</div>
+</template>
+<script>
+export default {
+    name:"Projects"
+}
+</script>
