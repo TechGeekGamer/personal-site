339356432403aec8e144247e1825d3f2138eb5e1
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-15 20:11:43 -0700

    Update OtherProjects.vue

--- src/components/OtherProjects.vue
+++ src/components/OtherProjects.vue
@@ -1,15 +1,6 @@
 <template>
-<div>
-  <b-card-group deck>
-
-    <b-card title="DeepSea CFW">
-    <b-card-img src="https://cdn.discordapp.com/attachments/703315673534693467/847563949725581322/3838195-ai_5.png" alt="DeepSea CFW Logo" class="p-0"></b-card-img>
-    <br>
-    <br>
-      <b-card-text>
-          Deepsea is a new All-in-One CFW package for the Nintendo Switch. You can learn more at <a href="https://github.com/Team-Neptune/DeepSea">https://github.com/Team-Neptune/DeepSea</a>
-      </b-card-text>
-    </b-card>
+<b-container>
+  <b-card-group rows>
 
     <b-card title="Korral-JS">
     <b-card-img src="https://cdn.discordapp.com/avatars/709980094717886529/c511814da6b09a742b652c89d335129c.webp?size=512" alt="BanSync Logo" class="w-25 rounded-circle"></b-card-img>
@@ -21,7 +12,7 @@
     </b-card>
     
   </b-card-group>
-</div>
+</b-container>
 </template>
 <script>
 export default {
