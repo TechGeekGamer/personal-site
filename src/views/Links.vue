b3629dbddb038e58f9c08d4bc54898a796a131be
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-16 00:14:46 -0700

    Update Links.vue

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -1,18 +1,18 @@
 <template>
   <b-container>
-    <h1>My Links</h1>
+    <h2>My Links</h2>
 
     <b-list-group style="max-width: 300px;" class="mx-auto">
       <b-list-group-item href="https://github.com/TechGeekGamer" class="d-flex align-items-center">
-        <b-avatar class="mr-3" src="/github-logo.png" style="background-color: white;"></b-avatar>
+        <b-img src="github-logo.png" class="mr-3 w-25"/>
         <span class="mr-auto">GitHub</span>
       </b-list-group-item>
       <b-list-group-item class="d-flex align-items-center" href="https://twitter.com/techgeekgamer">
-        <b-avatar variant="info" src="/twitter-logo.png" class="mr-3"></b-avatar>
+        <b-img src="/twitter-logo.png" class="mr-3 w-25"/>
         <span class="mr-auto">Twitter</span>
       </b-list-group-item>
       <b-list-group-item class="d-flex align-items-center" href="https://discord.com/users/461560462991949863">
-        <b-avatar variant="info" src="/discord-logo.png" class="mr-3" style="background-color: white;"></b-avatar>
+        <b-img src="/discord-logo.png" class="mr-3 w-25"/>
         <span class="mr-auto">Discord</span>
       </b-list-group-item>
     </b-list-group>
