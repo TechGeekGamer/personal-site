f8440df0cbe71447c409048581adbb518a14bd1f
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-15 20:14:45 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,30 @@
   <div class="home">
     <b-img v-bind="pfp" rounded="circle" alt="Circle image"></b-img>
     <br><br>
-    <h5>Programmer | High Schooler</h5>
+    <About/>
     <br>
-    <p>Click in the navigation above to see more</p>
+    <Projects/>
+    <br>
+    <OtherProjects/>
+    <br>
+    <Links/>
+    <br>
   </div>
 </template>
 
 <script>
-//import Projects from '../components/Projects.vue'
+import About from '../views/About.vue'
+import Projects from '../views/Projects.vue'
+import OtherProjects from '../views/OtherProjects.vue'
+import Links from '../views/Links.vue'
 export default {
   name: 'Home',
+  components:{
+    About,
+    Projects, 
+    OtherProjects,
+    Links
+  },
   data() {
     return {
         pfp: { blankColor: '#777', width: 150, height: 150, class: 'm1', src:"https://avatars.githubusercontent.com/u/46201432?v=2" }
