eb412de3de8802a2645a4582dfd69c2425152058
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-16 00:14:14 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="/about">About Me</b-nav-item>
-          <b-nav-item to="/projects">My Projects</b-nav-item>
-          <b-nav-item to="/other-projects">Other Projects</b-nav-item>
-          <b-nav-item to="/links">Links</b-nav-item>
-        </b-navbar-nav>
-        
-      </b-collapse>
-    </b-navbar>
     <br>
     <transition name="fade">
       <router-view/>
