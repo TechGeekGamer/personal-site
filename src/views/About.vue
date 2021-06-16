ba8d053737b8d1d8f432a96cbb9d550c67205141
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-16 00:14:17 -0700

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>About Me</h1>
+    <h2>About Me</h2>
     <p>Hey! I'm Daniel. I'm a high schooler and a developer. When I'm not in school, I usually work on Discord bots, and code random useless JS projects.</p>
   </div>
 </template>
