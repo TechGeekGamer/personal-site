7e8e21f4fc166e43e54dace934f0fb60ee5e0c90
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-06-26 13:37:21 -0700

    Update OtherProjects.vue

--- src/components/OtherProjects.vue
+++ src/components/OtherProjects.vue
@@ -2,7 +2,7 @@
 <b-container>
   <b-card-group rows>
 
-    <b-card title="Korral-JS">
+    <b-card title="Korral-JS" class="shadow">
     <b-card-img src="https://cdn.discordapp.com/avatars/709980094717886529/c511814da6b09a742b652c89d335129c.webp?size=512" alt="BanSync Logo" class="w-25 rounded-circle"></b-card-img>
     <br>
     <br>
