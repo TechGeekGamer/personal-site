cf5a0defdfb442881f0ed39c1614bb617e6a2ffe
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2022-03-27 17:59:34 -0700

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,3 @@
 <template>
-  <b-container>
-    <p>Hey, I'm Daniel! I'm a high schooler. When I'm not in school, I usually work on Discord bots, and code random useless JS/TS and VueJS projects.</p>
-  </b-container>
+  <p>Hey, I'm Daniel! I'm a high schooler. During school, I have a web development class. When I'm not in school, I usually work on Discord bots, code various JS/TS/VueJS projects, and talking to some people. I am planning to major in Computer Science when I go to college someday™️</p>
 </template>
